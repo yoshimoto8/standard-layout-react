fb1b3f423fc10f51d2ea14781757d12ce8fd71ef
yoshimoto8 <yoyoyo427435@gmail.com>
2019-04-29 18:20:11 +0900

    emotion setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={style} className="App">
+      a
     </div>
-  );
+  )
 }
 
-export default App;
+const style = css`
+  color: hotpink;
+`
